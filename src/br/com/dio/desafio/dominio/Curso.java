03c31c85fb92ee6bc6085b489bf118c1e7a39ea7
João Pedro Bastos <jpedrosantosb@gmail.com>
2022-08-05 20:49:39 -0300

    desafio dio

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,33 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso
+  extends Conteudo {
+
+  private Integer cargaHoraria;
+
+  public Curso(String titulo, String descricao, Integer cargaHoraria) {
+    super(titulo, descricao);
+    this.cargaHoraria = cargaHoraria;
+  }
+
+  
+  @Override
+  public double calcularXp() {
+    return XP_PADRAO * cargaHoraria;
+  }
+
+  public Integer getCargaHoraria() {
+    return cargaHoraria;
+  }
+
+  public void setCargaHoraria(Integer cargaHoraria) {
+    this.cargaHoraria = cargaHoraria;
+  }
+
+  @Override
+  public String toString() {
+    return "Curso{" + "titulo='" + getTitulo() + '\'' + ", descricao='"
+        + getDescricao() + '\'' + ", cargaHoraria=" + cargaHoraria + '}';
+  }
+
+}
